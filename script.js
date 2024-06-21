c4453de1fad143f1bcd176e31350f85d5526e483
Vivek Baskaran <vivek.baskaran@talentship.io>
2024-06-21 22:09:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(function() {
+    document.getElementById('display').textContent = '10';
+    setTimeout(function() {
+        document.getElementById('display').textContent = '9';
+        setTimeout(function() {
+            document.getElementById('display').textContent = '8';
+            setTimeout(function() {
+                document.getElementById('display').textContent = '7';
+                setTimeout(function() {
+                    document.getElementById('display').textContent = '6';
+                    setTimeout(function() {
+                        document.getElementById('display').textContent = '5';
+                        setTimeout(function() {
+                            document.getElementById('display').textContent = '4';
+                            setTimeout(function() {
+                                document.getElementById('display').textContent = '3';
+                                setTimeout(function() {
+                                    document.getElementById('display').textContent = '2';
+                                    setTimeout(function() {
+                                        document.getElementById('display').textContent = '1';
+                                        setTimeout(function() {
+                                            document.getElementById('display').textContent = 'Happy Independence Day';
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
